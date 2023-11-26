6dea5ca5fa2d228937d6ab04b23dcdd0af6d48f3
kbrackin <135669996+kbrackin@users.noreply.github.com>
2023-11-25 20:06:57 -0500

    Added js, not currently functioning

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,155 +1,155 @@
-// //DateJS for time conversions
-// dayjs().format();
+dayjs().format();
 
-// // Variables used outside of functions
-// let searchCount = 0;
-// let dayURL = "";
-// let loc = "";
-// let lat = "";
-// let lon = "";
-// let locURL = "";
-// let fiveDURL = "";
+// Global
+let searchCount = 0;
+let dayURL;
+let citySearch;
+let lat;
+let lon;
+let locationURL;
+let fiveDayURL;
+const weatherApiKey = '0fd5a9e8535340ed3a0f200988bebabc';
 
-// // loop to display items in local storage
-// $(document).ready(function () {
-//   for (let i = 0; i < localStorage.length; i++) {
-//     let key = localStorage.key(i);
-//     let savedsrc = localStorage.getItem(key);
-//     $(`.searched${key}`).text(savedsrc);
-//     searchCount = i;
-//   }
-//   if (searchCount > 0) {
-//     searchCount++;
-//   }
-// });
+// Show Local Storage
+$(document).ready(function () {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let savedsrc = localStorage.getItem(key);
+    $(`.searched${key}`).text(savedsrc);
+    searchCount = i;
+  }
+  if (searchCount > 0) {
+    searchCount++;
+  }
+});
 
-// // Start Search and add history tiles as each are created - starts to overwrite at #5
-// // $("#search").click(function ()
+// Start Search and add history tiles as each are created - starts to overwrite at #5
+$("#searchBtn").on('click', function () {
 
-// $("#citySelect").on("submit", function (event) {
+// $("#searchBtn").on("click", function (event) {
 //   // set buttons to overwrite when search boxes are full
-//   if (searchCount === 9) {
+//   if (searchCount === 5) {
 //     searchCount = 0;
-//   }
+//     }
+// })
 
-//   // create url for location lookup (lat/long)
-//   event.preventDefault();
-//   loc = $(`#cityInp`).val();
-//   $(`.searched${searchCount}`).text(loc);
-//   localStorage.setItem(searchCount, loc);
-//   searchCount++;
-//   locURL = `https://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=0fd5a9e8535340ed3a0f200988bebabc`;
-//   $(`#cityInp`).val("");
-//   $(`.card-body`).removeClass("invisible");
-//   locationSearch();
-// });
+  // create url for location lookup (lat/long)
+  event.preventDefault();
+  citySearch = $(`#citySearch`).val();
+  $(`.history${searchCount}`).text(citySearch);
+  localStorage.setItem(searchCount, citySearch);
+  searchCount++;
+  locationURL = `https://api.openweathermap.org/geo/1.0/direct?q=${citySearch}&limit=1&appid=${weatherApiKey}`;
+  $(`#citySearch`).val("");
+  $(`.card-body`).removeClass("invisible");
+  locationSearch();
+});
 
 // //Click function for history buttons
 // $(document).on(`click`, `.search`, function (event) {
-//   loc = $(this).text();
-//   locURL = `https://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=0fd5a9e8535340ed3a0f200988bebabc`;
+//   citySearch = $('#citySearch').text();
+//   locationURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityState}&limit=1&appid=${weatherApiKey}&units=imperial`;
 //   $(`.card-body`).removeClass("invisible");
 //   locationSearch();
 // });
 
-// // Location Search - URL created from input and pulls the lat and lon from it.
-// function locationSearch() {
-//   // send location lookup to api
-//   fetch(locURL)
-//     .then((response) => response.json())
+// Location Search - URL created from input and pulls the lat and lon from it.
+function locationSearch() {
+  fetch(locationURL)
+    .then((response) => response.json())
+    .then((location) => {
+      console.log(location);
 
-//     .then((location) => {
-//       console.log(location);
+      // Place current location on page
+    //   $(`#owLocation`).text(location[0].name + ", " + location[0].state);
+      lat = location[0].lat;
+      lon = location[0].lon;
 
-//       // Place current location on page
-//       $(`#owLocation`).text(location[0].name + ", " + location[0].state);
-//       lat = location[0].lat;
-//       lon = location[0].lon;
+      dayURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`;
+      fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial&exclude=current,minutely,hourly,alerts`;
+      dailySearch();
+      fiveDaySearch();
+    })
+    .catch((error) => {
+        console.log(error)
+      alert("Please enter a valid city name as City,State and try again.");
+      $(`.card-body`).addClass("invisible");
+    });
+}
 
-//       dayURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0fd5a9e8535340ed3a0f200988bebabc&units=imperial`;
-//       fiveDURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=0fd5a9e8535340ed3a0f200988bebabc&units=imperial&exclude=current,minutely,hourly,alerts`;
-//       dailySearch();
-//       fiveDaySearch();
-//     })
-//     .catch((error) => {
-//       alert("Please enter a valid city name as City,State and try again.");
-//       $(`.card-body`).addClass("invisible");
-//     });
-// }
+// Daily Stats
+function dailySearch() {
+  fetch(dayURL)
+    .then((response) => response.json())
+    .then((currentW) => {
+      $(`#dayDate`).text(`${dayjs.unix(currentW.dt).format("MM-DD-YYYY")}`);
+      $(`#dayTemp`).text(`Temp: ${currentW.main.temp}`);
+      $(`#dayWind`).text(`Wind: ${currentW.wind.speed}`);
+      $(`#dayHum`).text(`Humidity: ${currentW.main.humidity}`);
+      $("#dayIcon").attr(
+        "src",
+        `https://openweathermap.org/img/w/${currentW.weather[0].icon}.png`
+      );
+    });
+}
 
-// // Daily Stats
-// function dailySearch() {
-//   fetch(dayURL)
-//     .then((response) => response.json())
-//     .then((currentW) => {
-//       $(`#dayDate`).text(`${dayjs.unix(currentW.dt).format("MM-DD-YYYY")}`);
-//       $(`#dayTemp`).text(`Temp: ${currentW.main.temp}`);
-//       $(`#dayWind`).text(`Wind: ${currentW.wind.speed}`);
-//       $(`#dayHum`).text(`Humidity: ${currentW.main.humidity}`);
-//       $("#dayIcon").attr(
-//         "src",
-//         `https://openweathermap.org/img/w/${currentW.weather[0].icon}.png`
-//       );
-//     });
-// }
-
-// // 5 Days stats
-// function fiveDaySearch() {
-//   return fetch(fiveDURL)
-//     .then((response) => response.json())
-//     .then((fiveDay) => {
-//       let t = 4;
-//       // Day 1
-//       $(`#date1`).text(`${dayjs.unix(fiveDay.list[t].dt).format("MM-DD-YY")}`);
-//       $(`#temp1`).text(`Temp: ${fiveDay.list[t].main.temp}`);
-//       $(`#wind1`).text(`Wind: ${fiveDay.list[t].wind.speed}`);
-//       $(`#hum1`).text(`Humidity: ${fiveDay.list[t].main.humidity}`);
-//       $(`#icon1`).attr(
-//         "src",
-//         `https://openweathermap.org/img/w/${fiveDay.list[t].weather[0].icon}.png`
-//       );
-//       let t = 12;
-//       // Day 2
-//       $(`#date2`).text(`${dayjs.unix(fiveDay.list[t].dt).format("MM-DD-YY")}`);
-//       $(`#temp2`).text(`Temp: ${fiveDay.list[t].main.temp}`);
-//       $(`#wind2`).text(`Wind: ${fiveDay.list[t].wind.speed}`);
-//       $(`#hum2`).text(`Humidity: ${fiveDay.list[t].main.humidity}`);
-//       $(`#icon2`).attr(
-//         "src",
-//         `https://openweathermap.org/img/w/${fiveDay.list[t].weather[0].icon}.png`
-//       );
-//       let t = 20;
-//       // Day 3
-//       $(`#date3`).text(`${dayjs.unix(fiveDay.list[t].dt).format("MM-DD-YY")}`);
-//       $(`#temp3`).text(`Temp: ${fiveDay.list[t].main.temp}`);
-//       $(`#wind3`).text(`Wind: ${fiveDay.list[t].wind.speed}`);
-//       $(`#hum3`).text(`Humidity: ${fiveDay.list[t].main.humidity}`);
-//       $(`#icon3`).attr(
-//         "src",
-//         `https://openweathermap.org/img/w/${fiveDay.list[t].weather[0].icon}.png`
-//       );
-//       let t = 28;
-//       // Day 4
-//       $(`#date4`).text(`${dayjs.unix(fiveDay.list[t].dt).format("MM-DD-YY")}`);
-//       $(`#temp4`).text(`Temp: ${fiveDay.list[t].main.temp}`);
-//       $(`#wind4`).text(`Wind: ${fiveDay.list[t].wind.speed}`);
-//       $(`#hum4`).text(`Humidity: ${fiveDay.list[t].main.humidity}`);
-//       $(`#icon4`).attr(
-//         "src",
-//         `https://openweathermap.org/img/w/${fiveDay.list[t].weather[0].icon}.png`
-//       );
-//       let t = 36;
-//       // Day 5
-//       $(`#date5`).text(`${dayjs.unix(fiveDay.list[t].dt).format("MM-DD-YY")}`);
-//       $(`#temp5`).text(`Temp: ${fiveDay.list[t].main.temp}`);
-//       $(`#wind5`).text(`Wind: ${fiveDay.list[t].wind.speed}`);
-//       $(`#hum5`).text(`Humidity: ${fiveDay.list[t].main.humidity}`);
-//       $(`#icon5`).attr(
-//         "src",
-//         `https://openweathermap.org/img/w/${fiveDay.list[t].weather[0].icon}.png`
-//       );
-//     });
-// }
+// 5 Days stats
+function fiveDaySearch() {
+  return fetch(fiveDayURL)
+    .then((response) => response.json())
+    .then((fiveDay) => {
+      let apiT = 4;
+      // Day 1
+      $(`#day1`).text(`${dayjs.unix(fiveDay.list[apiT].dt).format("MM-DD-YY")}`);
+      $(`#temp1`).text(`Temp: ${fiveDay.list[apiT].main.temp}`);
+      $(`#wind1`).text(`Wind: ${fiveDay.list[apiT].wind.speed}`);
+      $(`#hum1`).text(`Humidity: ${fiveDay.list[apiT].main.humidity}`);
+      $(`#icon1`).attr(
+        "src",
+        `https://openweathermap.org/img/w/${fiveDay.list[apiT].weather[0].icon}.png`
+      );
+        apiT = 12;
+      // Day 2
+      $(`#day2`).text(`${dayjs.unix(fiveDay.list[apiT].dt).format("MM-DD-YY")}`);
+      $(`#temp2`).text(`Temp: ${fiveDay.list[apiT].main.temp}`);
+      $(`#wind2`).text(`Wind: ${fiveDay.list[apiT].wind.speed}`);
+      $(`#hum2`).text(`Humidity: ${fiveDay.list[apiT].main.humidity}`);
+      $(`#icon2`).attr(
+        "src",
+        `https://openweathermap.org/img/w/${fiveDay.list[apiT].weather[0].icon}.png`
+      );
+        apiT = 20;
+      // Day 3
+      $(`#day3`).text(`${dayjs.unix(fiveDay.list[apiT].dt).format("MM-DD-YY")}`);
+      $(`#temp3`).text(`Temp: ${fiveDay.list[apiT].main.temp}`);
+      $(`#wind3`).text(`Wind: ${fiveDay.list[apiT].wind.speed}`);
+      $(`#hum3`).text(`Humidity: ${fiveDay.list[apiT].main.humidity}`);
+      $(`#icon3`).attr(
+        "src",
+        `https://openweathermap.org/img/w/${fiveDay.list[apiT].weather[0].icon}.png`
+      );
+        apiT = 28;
+      // Day 4
+      $(`#day4`).text(`${dayjs.unix(fiveDay.list[apiT].dt).format("MM-DD-YY")}`);
+      $(`#temp4`).text(`Temp: ${fiveDay.list[apiT].main.temp}`);
+      $(`#wind4`).text(`Wind: ${fiveDay.list[apiT].wind.speed}`);
+      $(`#hum4`).text(`Humidity: ${fiveDay.list[apiT].main.humidity}`);
+      $(`#icon4`).attr(
+        "src",
+        `https://openweathermap.org/img/w/${fiveDay.list[apiT].weather[0].icon}.png`
+      );
+      apiT = 36;
+      // Day 5
+      $(`#day5`).text(`${dayjs.unix(fiveDay.list[apiT].dt).format("MM-DD-YY")}`);
+      $(`#temp5`).text(`Temp: ${fiveDay.list[apiT].main.temp}`);
+      $(`#wind5`).text(`Wind: ${fiveDay.list[apiT].wind.speed}`);
+      $(`#hum5`).text(`Humidity: ${fiveDay.list[apiT].main.humidity}`);
+      $(`#icon5`).attr(
+        "src",
+        `https://openweathermap.org/img/w/${fiveDay.list[apiT].weather[0].icon}.png`
+      );
+    });
+}
 
 
 
